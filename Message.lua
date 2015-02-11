2f97068ce5a2922a8ce25252535176de41bae400
camdenb <doctorshmoogle@gmail.com>
2015-02-10 18:38:25 -0800

    added new font and timer class

---
+++ Message.lua
@@ -0,0 +1,27 @@
+Message = Class{}
+
+function Message:init()
+
+	self.displayingMessage = false
+	self.currentMessage = ''
+
+end
+
+function Message:displayMessage(str, seconds)
+	self.displayingMessage = true
+	self.currentMessage = str
+	Timer.add(seconds, function() self:cancelMessage() end)
+end
+
+function Message:cancelMessage()
+	print('canceled')
+	self.displayingMessage = false
+end
+
+function Message:drawMessages()
+	if self.displayingMessage then
+		love.graphics.setFont(messageFont)
+		love.graphics.setColor(0,0,0)
+		printString(self.currentMessage, 10, WINDOW_HEIGHT - 25, true)
+	end
+end
